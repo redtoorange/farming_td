ac213853924d2c7a475a0f5c859744b6fb0e2e58
Andrew McGuiness <redtoorange@gmail.com>
2021-02-14 13:14:54 -0500

    ﻿Added 2D arrow projectile, revamped the tileset

--- Assets/Scripts/Buildings/Towers/Projectile.cs
+++ Assets/Scripts/Buildings/Towers/Projectile.cs
@@ -20,6 +20,12 @@ namespace Buildings.Towers
         public void SetTarget(Targetable target)
         {
             this.target = target;
+            Vector2 moveDirection = transform.position - target.GetAimPoint().position;
+            if (moveDirection != Vector2.zero)
+            {
+                float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector2.right);
+            }
         }
 
         private void FixedUpdate()
@@ -28,8 +34,15 @@ namespace Buildings.Towers
             {
                 if (updateThisFrame)
                 {
-                    Vector3 direction = target.GetAimPoint().position - transform.position;
-                    rigidbody.velocity = direction.normalized * speed;
+                    Vector2 dir = target.GetAimPoint().position - transform.position;
+                    if (dir != Vector2.zero)
+                    {
+                        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+                        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+                    }
+                    
+                    
+                    rigidbody.velocity = dir.normalized * speed;
                     updateThisFrame = false;
                 }
                 else
