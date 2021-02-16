5e72bd691f00e037fe609ef81e306a2dc21db522
Andrew McGuiness <redtoorange@gmail.com>
2021-02-16 18:59:21 -0500

    Test 3d fps controller

--- Assets/Scripts/Input/PlayerActions.cs
+++ Assets/Scripts/Input/PlayerActions.cs
@@ -124,6 +124,180 @@ namespace Input
                     ""isPartOfComposite"": false
                 }
             ]
+        },
+        {
+            ""name"": ""FPS"",
+            ""id"": ""38bf268e-a0e9-44d8-b673-ab82e319602d"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Value"",
+                    ""id"": ""7df968b2-d04b-49ed-989a-7d62cff55a0d"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""e7acab8f-9bbb-44c3-9768-02d3a648e935"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Sprint"",
+                    ""type"": ""Button"",
+                    ""id"": ""dae57e5e-edad-4c11-9789-58d9973b8e0c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""LookX"",
+                    ""type"": ""Value"",
+                    ""id"": ""7ddd58c6-ecdb-4843-bbd7-5774ebe938f3"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""LookY"",
+                    ""type"": ""Value"",
+                    ""id"": ""b0ccf47d-548d-42ca-b1e7-acc63e6868b4"",
+                    ""expectedControlType"": ""Axis"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""8fec73c2-fd8d-4917-8c22-ae385af937ae"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c1eaaf1f-bc20-42b7-beef-7c7b90e94e8c"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""367eb525-9ead-4e10-ad86-1ff7226ec747"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""d40f097b-7358-446c-9b7a-2f31aa22bf40"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""f266cee3-a3de-476a-9fba-631dbedd7602"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""16ac3934-9d5b-45c2-bcaf-a84f82fedc34"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9e4f14af-a606-478e-a3fd-0c7378d415f2"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Sprint"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""fcbce025-11a0-4be3-802e-aa1a89b6ec60"",
+                    ""path"": ""<Mouse>/delta/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LookX"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f7863d74-ccf9-4a1a-b545-9754167dfcb1"",
+                    ""path"": ""<Mouse>/delta/y"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LookY"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""RTS"",
+            ""id"": ""22bbb2a6-89a1-4a50-b4f4-82577014a3cc"",
+            ""actions"": [
+                {
+                    ""name"": ""New action"",
+                    ""type"": ""Button"",
+                    ""id"": ""6a045118-1231-491e-83e7-4da5e6925886"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""7eee40cb-de7f-42c7-9b99-a852fc655294"",
+                    ""path"": """",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""New action"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": [
@@ -139,6 +313,16 @@ namespace Input
             m_PlayerControls_CameraMove = m_PlayerControls.FindAction("CameraMove", throwIfNotFound: true);
             m_PlayerControls_CameraZoom = m_PlayerControls.FindAction("CameraZoom", throwIfNotFound: true);
             m_PlayerControls_CameraSprint = m_PlayerControls.FindAction("CameraSprint", throwIfNotFound: true);
+            // FPS
+            m_FPS = asset.FindActionMap("FPS", throwIfNotFound: true);
+            m_FPS_Movement = m_FPS.FindAction("Movement", throwIfNotFound: true);
+            m_FPS_Jump = m_FPS.FindAction("Jump", throwIfNotFound: true);
+            m_FPS_Sprint = m_FPS.FindAction("Sprint", throwIfNotFound: true);
+            m_FPS_LookX = m_FPS.FindAction("LookX", throwIfNotFound: true);
+            m_FPS_LookY = m_FPS.FindAction("LookY", throwIfNotFound: true);
+            // RTS
+            m_RTS = asset.FindActionMap("RTS", throwIfNotFound: true);
+            m_RTS_Newaction = m_RTS.FindAction("New action", throwIfNotFound: true);
         }
 
         public void Dispose()
@@ -233,6 +417,104 @@ namespace Input
             }
         }
         public PlayerControlsActions @PlayerControls => new PlayerControlsActions(this);
+
+        // FPS
+        private readonly InputActionMap m_FPS;
+        private IFPSActions m_FPSActionsCallbackInterface;
+        private readonly InputAction m_FPS_Movement;
+        private readonly InputAction m_FPS_Jump;
+        private readonly InputAction m_FPS_Sprint;
+        private readonly InputAction m_FPS_LookX;
+        private readonly InputAction m_FPS_LookY;
+        public struct FPSActions
+        {
+            private @PlayerActions m_Wrapper;
+            public FPSActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Movement => m_Wrapper.m_FPS_Movement;
+            public InputAction @Jump => m_Wrapper.m_FPS_Jump;
+            public InputAction @Sprint => m_Wrapper.m_FPS_Sprint;
+            public InputAction @LookX => m_Wrapper.m_FPS_LookX;
+            public InputAction @LookY => m_Wrapper.m_FPS_LookY;
+            public InputActionMap Get() { return m_Wrapper.m_FPS; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(FPSActions set) { return set.Get(); }
+            public void SetCallbacks(IFPSActions instance)
+            {
+                if (m_Wrapper.m_FPSActionsCallbackInterface != null)
+                {
+                    @Movement.started -= m_Wrapper.m_FPSActionsCallbackInterface.OnMovement;
+                    @Movement.performed -= m_Wrapper.m_FPSActionsCallbackInterface.OnMovement;
+                    @Movement.canceled -= m_Wrapper.m_FPSActionsCallbackInterface.OnMovement;
+                    @Jump.started -= m_Wrapper.m_FPSActionsCallbackInterface.OnJump;
+                    @Jump.performed -= m_Wrapper.m_FPSActionsCallbackInterface.OnJump;
+                    @Jump.canceled -= m_Wrapper.m_FPSActionsCallbackInterface.OnJump;
+                    @Sprint.started -= m_Wrapper.m_FPSActionsCallbackInterface.OnSprint;
+                    @Sprint.performed -= m_Wrapper.m_FPSActionsCallbackInterface.OnSprint;
+                    @Sprint.canceled -= m_Wrapper.m_FPSActionsCallbackInterface.OnSprint;
+                    @LookX.started -= m_Wrapper.m_FPSActionsCallbackInterface.OnLookX;
+                    @LookX.performed -= m_Wrapper.m_FPSActionsCallbackInterface.OnLookX;
+                    @LookX.canceled -= m_Wrapper.m_FPSActionsCallbackInterface.OnLookX;
+                    @LookY.started -= m_Wrapper.m_FPSActionsCallbackInterface.OnLookY;
+                    @LookY.performed -= m_Wrapper.m_FPSActionsCallbackInterface.OnLookY;
+                    @LookY.canceled -= m_Wrapper.m_FPSActionsCallbackInterface.OnLookY;
+                }
+                m_Wrapper.m_FPSActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Movement.started += instance.OnMovement;
+                    @Movement.performed += instance.OnMovement;
+                    @Movement.canceled += instance.OnMovement;
+                    @Jump.started += instance.OnJump;
+                    @Jump.performed += instance.OnJump;
+                    @Jump.canceled += instance.OnJump;
+                    @Sprint.started += instance.OnSprint;
+                    @Sprint.performed += instance.OnSprint;
+                    @Sprint.canceled += instance.OnSprint;
+                    @LookX.started += instance.OnLookX;
+                    @LookX.performed += instance.OnLookX;
+                    @LookX.canceled += instance.OnLookX;
+                    @LookY.started += instance.OnLookY;
+                    @LookY.performed += instance.OnLookY;
+                    @LookY.canceled += instance.OnLookY;
+                }
+            }
+        }
+        public FPSActions @FPS => new FPSActions(this);
+
+        // RTS
+        private readonly InputActionMap m_RTS;
+        private IRTSActions m_RTSActionsCallbackInterface;
+        private readonly InputAction m_RTS_Newaction;
+        public struct RTSActions
+        {
+            private @PlayerActions m_Wrapper;
+            public RTSActions(@PlayerActions wrapper) { m_Wrapper = wrapper; }
+            public InputAction @Newaction => m_Wrapper.m_RTS_Newaction;
+            public InputActionMap Get() { return m_Wrapper.m_RTS; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(RTSActions set) { return set.Get(); }
+            public void SetCallbacks(IRTSActions instance)
+            {
+                if (m_Wrapper.m_RTSActionsCallbackInterface != null)
+                {
+                    @Newaction.started -= m_Wrapper.m_RTSActionsCallbackInterface.OnNewaction;
+                    @Newaction.performed -= m_Wrapper.m_RTSActionsCallbackInterface.OnNewaction;
+                    @Newaction.canceled -= m_Wrapper.m_RTSActionsCallbackInterface.OnNewaction;
+                }
+                m_Wrapper.m_RTSActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @Newaction.started += instance.OnNewaction;
+                    @Newaction.performed += instance.OnNewaction;
+                    @Newaction.canceled += instance.OnNewaction;
+                }
+            }
+        }
+        public RTSActions @RTS => new RTSActions(this);
         private int m_KeyboardMouseSchemeIndex = -1;
         public InputControlScheme KeyboardMouseScheme
         {
@@ -247,6 +529,18 @@ namespace Input
             void OnCameraMove(InputAction.CallbackContext context);
             void OnCameraZoom(InputAction.CallbackContext context);
             void OnCameraSprint(InputAction.CallbackContext context);
+        }
+        public interface IFPSActions
+        {
+            void OnMovement(InputAction.CallbackContext context);
+            void OnJump(InputAction.CallbackContext context);
+            void OnSprint(InputAction.CallbackContext context);
+            void OnLookX(InputAction.CallbackContext context);
+            void OnLookY(InputAction.CallbackContext context);
+        }
+        public interface IRTSActions
+        {
+            void OnNewaction(InputAction.CallbackContext context);
         }
     }
 }
